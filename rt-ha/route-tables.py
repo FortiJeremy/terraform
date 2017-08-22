aa56e45160f808f2286b3f0c63644c3b71d6187e
Jeremy Furtney <jfurtney@fortinet.com>
2017-08-22 13:37:04 -0400

    fixed script from old version

--- rt-ha/route-tables.py
+++ rt-ha/route-tables.py
@@ -1,7 +1,9 @@
 import boto3
+import os
 from boto3.session import Session
-region = ‘region’
-rts = { ‘route_table_1’, ‘route_table_2’ }
+
+region = os.environ['ha_region']
+rts = { os.environ['route_table_1', os.environ['route_table_2'] }
 def get_healthy_rt (troubled_rt_id):
  for rt_id in rts:
    if rt_id != troubled_rt_id:
@@ -11,16 +13,16 @@ def get_healthy_rt (troubled_rt_id):
 def do (event, context):
   
  session = Session(region_name=region)
- ec2 = session.resource(‘ec2’)
+ ec2 = session.resource('ec2')
  for rt_id in rts:
    rt = ec2.RouteTable(rt_id)
    for route in rt.routes:
-     if (route[‘State’])==’blackhole’:
-       print (“affected route table found: “ + rt_id) 
+     if route.state=='blackhole':
+       print ("affected route table found: " + rt_id) 
        for assoc_attr in rt.associations_attribute:
-         assoc = ec2.RouteTableAssociation(assoc_attr[‘RouteTableAssociationId’]) 
+         assoc = ec2.RouteTableAssociation(assoc_attr['RouteTableAssociationId']) 
          healthy_rt = get_healthy_rt (rt_id)
          new_assoc = assoc.replace_subnet(DryRun=False,RouteTableId = healthy_rt )
-         print (“troubled route table: “ + rt_id +” has been replaced with: “ + healthy_rt )
+         print ("troubled route table: " + rt_id + " has been replaced with: " + healthy_rt )
  
  return True
